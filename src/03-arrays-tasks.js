cc5480646a72903f46b19447b366c39ca73b7f80
Evgenii Kolesnikov <jekow@yandex.ru>
2022-11-30 20:13:25 +0300

    feat: implement 79 passing, 19 pending

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -545,8 +545,11 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const map = new Map();
+  array.map((item) => map
+    .set(keySelector(item), [...(map.get(keySelector(item)) || []), valueSelector(item)]));
+  return map;
 }
 
 
@@ -563,8 +566,10 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr
+    .map((v) => childrenSelector(v))
+    .reduce((sum, v) => [...sum, ...v], []);
 }
 
 
@@ -580,8 +585,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((sum, v) => sum[v], arr);
 }
 
 
@@ -603,8 +608,12 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const index = Math.floor(arr.length / 2) - 1;
+  const head = index >= 0 ? arr.slice(0, arr[index]) : [];
+  const tail = arr.length % 2 === 0 ? arr.slice(arr[index]) : arr.slice(arr[index + 1]);
+  const center = arr.length % 2 !== 0 ? arr.slice(arr[index], arr[index + 1]) : [];
+  return [...tail, ...center, ...head];
 }
 
 
